ac3731248f762c61835d0b41084a1ce599b763a1
ShreeVarshana <shreevarshanar.22cse@kongu.edu>
2025-04-19 08:06:31 +0530

    "change the index.js component to run the taks-1"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Appp from './Task-1/App';
+import './index.css'
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Appp />
+    <App />
   </React.StrictMode>
 );
 
